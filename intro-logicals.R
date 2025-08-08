9417cf67d78977933e42b6f74d39cd5e9858c505
ht-oliver <holiver327@gmail.com>
2025-08-08 13:53:50 -0700

    logicals practice

---
+++ intro-logicals.R
@@ -0,0 +1,31 @@
+rm(list = ls())
+
+dogs <- c(1, 10, 4, 5, 18)
+cats <- c(0, 12, 10, 5, 2)
+
+dogs <= cats
+
+5 != 6
+
+
+
+"cat" != "dog"
+
+#Logical and's
+5 < 10 & "cat" < "dog"
+
+9 > 4 & 2 < 3
+
+bananas <- c(10, 21, 12, 15, 22)
+apples <- c(9, 18, 16, 20, 30)
+
+apples < bananas & bananas > 20
+
+#Logical or's
+
+4 < 10 | 100 < 2
+5 > 6 | 'dog' < 'cat' | 10 < 50
+
+# Logical "NOT"
+!'dog' == 'cat'
+! 6 == 5
